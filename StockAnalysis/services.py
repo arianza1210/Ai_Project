0e5bf7a658a2d170cb9b0056b28b5ed067009c66
arianza1210 <nagatoarianza12@gmail.com>
2025-12-13 19:58:38 +0700

    add cors handler

--- StockAnalysis/services.py
+++ StockAnalysis/services.py
@@ -21,10 +21,17 @@ security = HTTPBearer()
 # =========================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://*.vercel.app",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=[
+        "Authorization",
+        "Content-Type",
+        "Accept",
+    ],
 )
 
 # =========================
